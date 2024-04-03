c6b4977f47a35c21f8a7011def717b25cb8261aa
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-04-03 14:28:13 +0200

    added type project controller

---
+++ app/Http/Controllers/Api/TypeProjectController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use App\Models\Type;
+use Illuminate\Http\Request;
+
+class TypeProjectController extends Controller
+{
+    public function __invoke(string $slug)
+    {
+        $type = Type::whereSlug($slug)->first();
+        if (!$type) return response(null, 404);
+
+        $projects = Project::whereTypeId($type->id)->whereIsCompleted(true)->with('type', 'technologies')->get();
+
+        return response()->json(['projects' => $projects, 'label' => $type->label]);
+    }
+}
